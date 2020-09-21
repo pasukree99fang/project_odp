3a24f65d61a437281d8881dbc33773ada28814c4
pasukree99fang <58066321+pasukree99fang@users.noreply.github.com>
2020-09-21 15:50:24 +0700

    Delete connectdb.php

--- connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("db-odp.mysql.database.azure.com","adminodp@db-odp","60admin.","odp");
-
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-?>
