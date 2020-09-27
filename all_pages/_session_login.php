c28485f6d44afebe081b8a7d7093b3d877f286d6
pasukree99fang <58066321+pasukree99fang@users.noreply.github.com>
2020-09-27 22:42:47 +0700

    update

--- all_pages/_session_login.php
+++ all_pages/_session_login.php
@@ -1,7 +1,7 @@
 <?php
-	session_start();
-	include 'connectdb.php';
-	if ($_SESSION['us_username'] != null)
-	 echo "<script>alert('HELLO'); window.location.href='../_home.php';</script>";
+	// session_start();
+	// include 'connectdb.php';
+	// if ($_SESSION['us_username'] != null)
+	//  echo "<script>alert('HELLO'); window.location.href='../_home.php';</script>";
 	
 ?> 
